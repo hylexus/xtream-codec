47cc2e9c19877780a954742e20f84ae94aae7e3b
hylexus <hylexus@163.com>
2025-11-16 19:39:28 +0800

    build[actions]: ...

--- build-script/maven/publish-to-maven-repository-github-packages.sh
+++ build-script/maven/publish-to-maven-repository-github-packages.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env sh
 
 XTREAM_PROJECT_ROOT_DIR=$(cd "$(dirname "$0")/../.."; pwd)
-DASHBOARD_808_PROJECT_DIR="${XTREAM_PROJECT_ROOT_DIR}/ext/jt/jt-808-server-dashboard-spring-boot-starter-reactive"
 echo "XTREAM_PROJECT_ROOT_DIR  : ${XTREAM_PROJECT_ROOT_DIR}"
-echo "DASHBOARD_808_PROJECT_DIR: ${DASHBOARD_808_PROJECT_DIR}"
-
 
 cd ${XTREAM_PROJECT_ROOT_DIR}
 echo "Working-Directory        : ${XTREAM_PROJECT_ROOT_DIR}"
